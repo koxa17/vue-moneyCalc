3610333f843509cdf18827de364298101083d220
koxa17 <n.matusevich@clicktoid.ru>
2022-05-10 16:46:23 +0500

    pages added vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-moneyCalc/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-moneyCalc/' : '/',
+  pages: {
+    index: {
+      entry: 'src/index/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+    },
+  }
 };
