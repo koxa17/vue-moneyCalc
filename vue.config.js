361aef7edd7b2c427b6f0df0412c5ad42525b08d
koxa17 <n.matusevich@clicktoid.ru>
2022-05-10 16:54:09 +0500

    delete pages vue config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-moneyCalc/' : '/',
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'Index Page',
-    },
-  }
 };
